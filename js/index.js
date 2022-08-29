559b8ce59e50b228e8baefbaa2a3aaad22b6bb0e
Kamil Konopski <Kamil.Konopski.2000@gmail.com>
2022-08-30 00:21:56 +0200

    add dark mode

--- js/index.js
+++ js/index.js
@@ -1,7 +1,20 @@
 const toggleThemeButton = document.querySelector('.header__theme-button');
+const header = document.querySelector('.header');
+const dashboardItems = document.querySelectorAll('.dashboard__item');
+const overviewItems = document.querySelectorAll('.overview__item');
+const overviewTitle = document.querySelector('.overview__title');
 
 const toggleTheme = () => {
-    toggleThemeButton.classList.toggle('header__theme-button--dark-mode')
+    toggleThemeButton.classList.toggle('header__theme-button--dark-mode');
+    document.body.classList.toggle('dark');
+    header.classList.toggle('dark');
+    dashboardItems.forEach(item => {
+        item.classList.toggle('dark');
+    });
+    overviewTitle.classList.toggle('dark');
+    overviewItems.forEach(item => {
+        item.classList.toggle('dark');
+    });
 };
 
 toggleThemeButton.addEventListener('click', toggleTheme);
